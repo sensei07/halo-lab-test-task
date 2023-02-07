33af4cd268059612289e28c88fbd563494504451
sensei07 <atomkiv17@gmail.com>
2023-02-07 10:45:04 +0200

    added section aaboutTeam nd  tyled it

--- data/constants/index.ts
+++ data/constants/index.ts
@@ -3,11 +3,44 @@ interface INavItems {
     link: string
 }
 
+interface ITeamItems {
+    id: number,
+    imageUrl: string,
+    width: number,
+    height: number,
+    alt: string
+}
+
 export const NAV_ITEMS: INavItems[] = [
     {name: 'Home', link: '/'},
     {name: 'Our mission', link: '/our-mission'},
     {name: 'Places', link: '/places'},
     {name: 'Team', link: 'team'}
+];
+
+
+export const TEAM_ITEMS: ITeamItems[] = [
+    {
+        id: 1,
+        imageUrl: '/images/team/team1.png',
+        width: 323,
+        height: 274,
+        alt: 'person 1'
+    },
+    {
+        id: 2,
+        imageUrl: '/images/team/team2.png',
+        width: 334,
+        height: 263,
+        alt: 'person 2',
+    },
+    {
+        id: 3,
+        imageUrl: '/images/team/team3.png',
+        width: 276,
+        height: 312,
+        alt: 'person 2',
+    }
 ];
 
 
